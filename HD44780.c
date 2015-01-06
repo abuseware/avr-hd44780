fde6db22d4e1904974c7181f43715ff7a85cd307
Licho <abuseware@users.noreply.github.com>
2015-01-06 16:41:31 +0100

    Cleanup

--- HD44780.c
+++ HD44780.c
@@ -6,7 +6,7 @@
 #include "HD44780.h"
 
 static void lcd_write_nibble(uint8_t data){
-  uint8_t i, bit;
+  uint8_t i;
 
   for(i = 0; i < 4; i++){
     if(((data >> i) & 1) == 1){
@@ -17,7 +17,6 @@ static void lcd_write_nibble(uint8_t data){
   }
 }
 static void lcd_write(uint8_t data){
-  //pcf8574_pin_on(HD44780_ADDR, HD44780_E);
   lcd_write_nibble(data >> 4);
   pcf8574_pin_blink(HD44780_ADDR, HD44780_E);
   
@@ -58,21 +57,19 @@ void lcd_home(void){
 void lcd_init(void){
   uint8_t i;
 
-  pcf8574_pin_off(HD44780_ADDR, HD44780_RS); // wyzerowanie linii RS
-  pcf8574_pin_off(HD44780_ADDR, HD44780_E);  // wyzerowanie linii E
   for(i = 0; i < 3; i++) {
-    lcd_write_nibble(0x03); // tryb 8-bitowy
+    lcd_write_nibble(0x03);
     pcf8574_pin_blink(HD44780_ADDR, HD44780_E);
-    _delay_ms(5); // czekaj 5ms
+    _delay_ms(5);
   }
 
-  lcd_write_nibble(0x02); // tryb 4-bitowy
+  lcd_write_nibble(0x02);
   pcf8574_pin_blink(HD44780_ADDR, HD44780_E);
-  _delay_ms(2); // czekaj 1ms 
+  _delay_ms(2);
   
-  lcd_write_command(HD44780_FUNCTION_SET | HD44780_FONT5x7 | HD44780_TWO_LINE | HD44780_4_BIT); // interfejs 4-bity, 2-linie, znak 5x7
-  lcd_write_command(HD44780_DISPLAY_ONOFF | HD44780_DISPLAY_OFF); // wyłączenie wyswietlacza
-  lcd_write_command(HD44780_CLEAR); // czyszczenie zawartości pamieci DDRAM
-  lcd_write_command(HD44780_ENTRY_MODE | HD44780_EM_SHIFT_CURSOR | HD44780_EM_INCREMENT);// inkrementaja adresu i przesuwanie kursora
-  lcd_write_command(HD44780_DISPLAY_ONOFF | HD44780_DISPLAY_ON | HD44780_CURSOR_OFF | HD44780_CURSOR_NOBLINK); // włącz LCD, bez kursora i mrugania
+  lcd_write_command(HD44780_FUNCTION_SET | HD44780_FONT5x7 | HD44780_TWO_LINE | HD44780_4_BIT);
+  lcd_write_command(HD44780_DISPLAY_ONOFF | HD44780_DISPLAY_OFF);
+  lcd_write_command(HD44780_CLEAR);
+  lcd_write_command(HD44780_ENTRY_MODE | HD44780_EM_SHIFT_CURSOR | HD44780_EM_INCREMENT);
+  lcd_write_command(HD44780_DISPLAY_ONOFF | HD44780_DISPLAY_ON | HD44780_CURSOR_OFF | HD44780_CURSOR_NOBLINK);
 }
